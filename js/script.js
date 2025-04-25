b8c4af068b3a8b50f9cdfd8ac312ea0fd7a97293
makinomomoka <167516913+makinomomoka@users.noreply.github.com>
2025-04-25 18:39:09 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("myButton").addEventListener("click", function() {
+    //   alert("もう少ししたら授業終わるよ");
+    // });
+    
+    $(function() {
+      $("#myButton").click(function() {
+        alert("ボタンを押したぞ！");
+      });
+    });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
